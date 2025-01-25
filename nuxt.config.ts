0a5264dd3cdc1762c96e7f249b94198d44f11db1
William Chong <me@williamchong.cloud>
2025-01-26 03:56:50 +0800

    ➕ Add Tailwind CSS module to Nuxt configuration and update package dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,5 +15,5 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss']
 })
